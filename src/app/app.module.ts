2f0b859e204ac05d47a45078701a6834ebc2474b
Bastien-Arlot <arlot.b@gmail.com>
2022-08-20 10:26:15 +0200

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { UsersComponent } from './users/users.component';
-import { RouterModule, Routes } from "@angular/router";
+import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 const APP_ROUTES: Routes = [
   { path: '', component: HomepageComponent },
-  { path: 'users', component: UsersComponent }
+  { path: 'users', component: UsersComponent },
+  { path: 'users/:id/:name', component: UserComponent },
 ];
 
 @NgModule({
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(APP_ROUTES)],
-  declarations: [AppComponent, HomepageComponent, UsersComponent],
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    UsersComponent,
+    UserComponent,
+  ],
   bootstrap: [AppComponent],
-  providers: []
+  providers: [],
 })
 export class AppModule {}
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
+interface User {
+  id: number;
+  name: string;
+}
 @Component({
-  selector: "app-users",
-  templateUrl: "./users.component.html",
-  styleUrls: ["./users.component.scss"]
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
+  public users: User[] = [
+    { id: 1, name: 'jean' },
+    { id: 2, name: 'Julie' },
+    { id: 3, name: 'Pierre' },
+  ];
   constructor() {}
 
   ngOnInit() {}
