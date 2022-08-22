16a4394133fbf3d9b218a8a2fc7b1d45c2361394
Bastien-Arlot <arlot.b@gmail.com>
2022-08-22 17:54:56 +0200

    2 files modified

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -15,7 +15,10 @@ export class UserComponent implements OnInit {
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
       this.id = paramMap.get('id');
+    });
+    this.activatedRoute.queryParamMap.subscribe((paramMap: ParamMap) => {
       this.name = paramMap.get('name');
     });
+
   }
 }
