a3a28c876316b5917be7afc6c9694d582b511bd3
Bastien-Arlot <arlot.b@gmail.com>
2022-08-22 18:24:12 +0200

    1 file modified

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,7 +9,7 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 export class UserComponent implements OnInit {
   public id!: string | null;
   public name!: string | null;
-  public details: string;
+  public details!: string | null;
 
   constructor(private activatedRoute: ActivatedRoute) {}
 
